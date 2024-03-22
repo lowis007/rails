f2cc97440f1edb6056a550a92f5e75ad3b19677d
lowis007 <matthewlouw2@gmail.com>
2024-03-22 09:15:52 +0000

    Remove redundancies and refactoring

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,50 +1,54 @@
 class TodosController < ApplicationController
-    def new
-			@todo = Todo.new
-    end
+  before_action :set_todo, only: [:edit, :update, :show, :destroy]
+  def new
+    @todo = Todo.new
+  end
 
-		def create
-      @todo = Todo.new(todo_params)
-      if @todo.save
-        flash[:notice]="Todo was created successfully"
-        redirect_to todo_path(@todo)
-      else
-        render :new, status: :unprocessable_entity
-      end
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      flash[:notice]="Todo was created successfully"
+      redirect_to todo_path(@todo)
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
+  
+  def show
     
-    def show
-      @todo=Todo.find(params[:id])
-    end
+  end
 
-    def edit
-      @todo=Todo.find(params[:id])  
-    end
+  def edit
+      
+  end
 
-    def update
-      @todo=Todo.find(params[:id])
-      if @todo.update(todo_params)
-        flash[:notice] = "Todo was successfully updated"
-        redirect_to todo_path(@todo)
-      else
-        render :edit, status: :unprocessable_entity 
-      end
-    end
-
-    def index
-      @todos = Todo.all 
-    end
+  def update
     
-    def destroy
+    if @todo.update(todo_params)
+      flash[:notice] = "Todo was successfully updated"
+      redirect_to todo_path(@todo)
+    else
+      render :edit, status: :unprocessable_entity 
+    end
+  end
+
+  def index
+    @todos = Todo.all 
+  end
+  
+  def destroy
+    
+    @todo.destroy
+    flash[:notice]="Todo was deleted successfully"
+    redirect_to todos_path
+  end
+
+  private
+    def set_todo
       @todo = Todo.find(params[:id])
-      @todo.destroy
-      flash[:notice]="Todo was deleted successfully"
-      redirect_to todos_path
     end
 
-    private
-    
-      def todo_params
-        params.require(:todo).permit(:name,:description)
-      end
+    def todo_params
+      params.require(:todo).permit(:name,:description)
+    end
 end
